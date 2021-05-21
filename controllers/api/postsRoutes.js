165c2cce1e859d2415804eef21bd15c0a765116c
Dmitry <dmitry.balduev@gmail.com>
2021-05-21 18:56:39 -0400

    edited routes

--- controllers/api/postsRoutes.js
+++ controllers/api/postsRoutes.js
@@ -1,15 +1,30 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.findAll({
+      include:{
+        model: User,
+        model: Comment
+      }
+    });
+
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newPost = await Project.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newPost);
   } catch (err) {
     res.status(400).json(err);
   }
